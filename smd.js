4d8587de25bfc2c2cf1e5da1e3bc1c9a9aee45cd
Sophivorus <schenonef@gmail.com>
2025-06-09 09:34:36 -0500

    Use async, exclude automatic translations, use local storage, and other fixes and improvements

--- smd.js
+++ smd.js
@@ -11,14 +11,14 @@ const SMD = {
 			SMD.fetchSimulations()
 		] ).then( SMD.finishMainPage );
 
-		// Get the bookmarks from the cookies and update the button
-		const bookmarks = SMD.getCookie( 'smd-bookmarks' );
+		// Get the bookmarks from the local storage and update the button
+		const bookmarks = window.localStorage.getItem( 'smd-bookmarks' );
 		if ( bookmarks ) {
 			SMD.bookmarks = JSON.parse( bookmarks );
 		}
 		SMD.updateBookmarksButton();
 
-		// Enable installing the app the device supports it
+		// Enable installing the app if the device supports it
 		window.onbeforeinstallprompt = SMD.enableInstallButton;
 
 		// Bind events
@@ -38,6 +38,8 @@ const SMD = {
 		bookmarksDialogContent.onclick = event => event.stopPropagation();
 		const bookmarksDialogCloseButton = document.getElementById( 'smd-bookmarks-dialog-close-button' );
 		bookmarksDialogCloseButton.onclick = () => bookmarksDialog.close();
+		const bookmarksDialogDownloadButton = document.getElementById( 'smd-bookmarks-dialog-download-button' );
+		bookmarksDialogDownloadButton.onclick = SMD.downloadPDF;
 		const tissueDialog = document.getElementById( 'smd-tissue-dialog' );
 		tissueDialog.onclick = tissueDialog.close;
 		const tissueDialogContent = document.getElementById( 'smd-tissue-dialog-content' );
@@ -87,10 +89,7 @@ const SMD = {
 		const index = SMD.bookmarks.findIndex( bookmark => bookmark.href === data.href );
 		star.innerHTML = index === -1 ? '&#9734;' : '&#9733;';
 		star.title = index === -1 ? 'Click to bookmark this ' + data.type : 'Click to remove this bookmark';
-		star.dataset.href = data.href;
-		star.dataset.text = data.text;
-		star.dataset.type = data.type;
-		star.onclick = SMD.toggleBookmark;
+		star.onclick = () => SMD.toggleBookmark( event, data );
 	},
 
 	switchTab: function ( event ) {
@@ -111,29 +110,24 @@ const SMD = {
 		simulationsTable.style.display = tab.id === 'smd-simulations-tab' ? 'block' : 'none';
 	},
 
-	toggleBookmark: function ( event ) {
+	toggleBookmark: function ( event, data ) {
 
 		// Toggle the star
 		const star = event.target;
-		const index = SMD.bookmarks.findIndex( bookmark => bookmark.href === star.dataset.href );
+		const index = SMD.bookmarks.findIndex( bookmark => bookmark.href === data.href );
 		if ( index === -1 ) {
-			const bookmark = {
-				type: star.dataset.type,
-				text: star.dataset.text,
-				href: star.dataset.href
-			};
-			SMD.bookmarks.push( bookmark );
+			SMD.bookmarks.push( data );
 			star.innerHTML = '&#9733;';
 			star.title = 'Click to remove this bookmark';
 		} else {
 			SMD.bookmarks.splice( index, 1 );
 			star.innerHTML = '&#9734;';
-			star.title = 'Click to bookmark this ' + star.dataset.type;
+			star.title = 'Click to bookmark this ' + data.type;
 		}
 
-		// Update the cookie
+		// Update the local storage
 		const bookmarks = JSON.stringify( SMD.bookmarks );
-		document.cookie = 'smd-bookmarks=' + bookmarks + '; path=/';
+		window.localStorage.setItem( 'smd-bookmarks', bookmarks );
 
 		// Update the button
 		SMD.updateBookmarksButton();
@@ -145,7 +139,7 @@ const SMD = {
 		button.removeAttribute( 'hidden' );
 		button.onclick = () => {
 			event.prompt();
-			button.setAttribute(  'hidden', '' );
+			button.setAttribute( 'hidden', '' );
 		};
 	},
 
@@ -199,7 +193,7 @@ const SMD = {
 		}
 	},
 
-	openTissueDialog: function ( event ) {
+	openTissueDialog: async function ( event ) {
 		const tissueName = event.target.textContent;
 
 		// Show the dialog
@@ -211,64 +205,64 @@ const SMD = {
 		content.textContent = 'Loading...';
 
 		// Fetch the rest of the data and then finish the dialog
-		Promise.all( [
+		const data = await Promise.all( [
 			SMD.fetchTissue( tissueName ),
 			SMD.fetchTissueSimulations( tissueName )
-		] ).then( data => {
-			const tissue = data[0];
-			const simulations = data[1];
+		] );
 
-			// Clone the template and add it to the dialog
-			const template = document.getElementById( 'smd-tissue-dialog-template' );
-			const templateContent = template.content.cloneNode( true );
-			content.textContent = '';
-			content.append( templateContent );
+		const tissue = data[0];
+		const simulations = data[1];
 
-			// Bind the close button
-			const button = dialog.querySelector( 'button' );
-			button.onclick = () => dialog.close();
+		// Clone the template and add it to the dialog
+		const template = document.getElementById( 'smd-tissue-dialog-template' );
+		const templateContent = template.content.cloneNode( true );
+		content.textContent = '';
+		content.append( templateContent );
 
-			// Finish the main heading
-			const heading = dialog.querySelector( 'h2' );
-			heading.textContent = 'Tissue: ' + tissue.name;
+		// Bind the close button
+		const button = dialog.querySelector( 'button' );
+		button.onclick = () => dialog.close();
 
-			// Finish the image
-			const image = dialog.querySelector( 'img' );
-			if ( tissue.image ) {
-				image.src = tissue.image.source;
-				image.width = tissue.image.width;
-				image.height = tissue.image.height;
-			} else {
-				image.remove();
+		// Finish the main heading
+		const heading = dialog.querySelector( 'h2' );
+		heading.textContent = 'Tissue: ' + tissue.name;
+
+		// Finish the image
+		const image = dialog.querySelector( 'img' );
+		if ( tissue.image ) {
+			image.src = tissue.image.source;
+			image.width = tissue.image.width;
+			image.height = tissue.image.height;
+		} else {
+			image.remove();
+		}
+
+		// Finish the about section
+		const about = dialog.querySelector( 'p' );
+		if ( tissue.about ) {
+			about.textContent = tissue.about;
+		} else {
+			about.remove();
+		}
+
+		// Finish the simulations section
+		const list = dialog.querySelector( 'ul' );
+		if ( simulations.length > 0 ) {
+			list.textContent = ''; // Empty the list
+			for ( const simulation of simulations ) {
+				const template = document.getElementById( 'smd-explore-list-item' );
+				const item = template.content.cloneNode( true ).children[0];
+				const link = item.querySelector( 'a' );
+				link.textContent = simulation.printouts['SMD material'].join( ' + ' );
+				link.href = simulation.href;
+				SMD.finishStar( item, simulation );
+				list.append( item );
 			}
-
-			// Finish the about section
-			const about = dialog.querySelector( 'p' );
-			if ( tissue.about ) {
-				about.textContent = tissue.about;
-			} else {
-				about.remove();
-			}
-
-			// Finish the simulations section
-			const list = dialog.querySelector( 'ul' );
-			if ( simulations.length > 0 ) {
-				list.textContent = ''; // Empty the list
-				for ( const simulation of simulations ) {
-					const template = document.getElementById( 'smd-explore-list-item' );
-					const item = template.content.cloneNode( true ).children[0];
-					const link = item.querySelector( 'a' );
-					link.textContent = simulation.printouts['SMD material'].join( ' + ' );
-					link.href = simulation.href;
-					SMD.finishStar( item, simulation );
-					list.append( item );
-				}
-				const radar = dialog.querySelector( 'canvas' );
-				SMD.updateTissueRadar( radar, simulations );
-			} else {
-				list.textContent = 'No simulations for this tissue yet.';
-			}
-		} );
+			const radar = dialog.querySelector( 'canvas' );
+			SMD.updateTissueRadar( radar, simulations );
+		} else {
+			list.textContent = 'No simulations for this tissue yet.';
+		}
 	},
 
 	updateTissueRadar: function ( radar, simulations ) {
@@ -294,102 +288,93 @@ const SMD = {
 				olfactory++;
 			}
 		}
-		if ( visual + tactile + manipulation + auditory + olfactory > 0 ) {
-			new Chart( radar, {
-				type: 'radar',
-				data: {
-					labels: [ 'Visual', 'Tactile', 'Manipulation', 'Auditory', 'Olfactory' ],
-					datasets: [ {
-						data: [ visual, tactile, manipulation, auditory, olfactory ]
-					} ],
-				},
-				options: {
-					responsive: false,
-					scales: {
-						r: {
-							ticks: {
-								stepSize: 1
-							}
-						}
-					},
-					plugins: {
-						legend: {
-							display: false
+		if ( visual + tactile + manipulation + auditory + olfactory === 0 ) {
+			radar.remove();
+			return;
+		}
+		new Chart( radar, {
+			type: 'radar',
+			data: {
+				labels: [ 'Visual', 'Tactile', 'Manipulation', 'Auditory', 'Olfactory' ],
+				datasets: [ {
+					data: [ visual, tactile, manipulation, auditory, olfactory ]
+				} ],
+			},
+			options: {
+				responsive: false,
+				scales: {
+					r: {
+						ticks: {
+							stepSize: 1
 						}
 					}
+				},
+				plugins: {
+					legend: {
+						display: false
+					}
 				}
-			} );
-		} else {
-			radar.remove();
-		}
+			}
+		} );
 	},
 
-	fetchTissues: function () {
-		const params = new URLSearchParams( {
-			origin: '*',
-			format: 'json',
-			formatversion: 2,
+	fetchTissues: async function () {
+		const params = {
 			action: 'query',
-			generator: 'categorymembers',
-			gcmtitle: 'Category:SMD tissues',
-			gcmnamespace: 0,
-			gcmlimit: 'max',
+			generator: 'search',
+			gsrsearch: 'incategory:"SMD tissues" -incategory:"Automatic translations"',
+			gsrlimit: 'max',
 			prop: 'info',
 			inprop: 'url'
-		} );
-		const url = 'https://www.appropedia.org/w/api.php?' + params.toString();
-		return fetch( url ).then( response => response.json() ).then( response => {
-			const results = response.query.pages;
-			const tissues = [];
-			for ( const result of results ) {
-				const tissue = {
-					type: 'tissue',
-					href: result.fullurl,
-					text: result.title.replace( 'SMD/Tissues/', '' )
-				}
-				tissues.push( tissue );
+		};
+		const data = await SMD.get( params );
+		const results = data.query.pages;
+		const tissues = [];
+		for ( const result of results ) {
+			const tissue = {
+				type: 'tissue',
+				href: result.fullurl,
+				title: result.title,
+				text: result.title.replace( 'SMD/Tissues/', '' )
 			}
-			// Sort alphabetically by text
-			tissues.sort( function ( a, b ) {
-				return ( a.text < b.text ) ? -1 : ( a.text > b.text ) ? 1 : 0;
-			} );
-			SMD.tissues = tissues;
+			tissues.push( tissue );
+		}
+		// Sort alphabetically by text
+		tissues.sort( function ( a, b ) {
+			return ( a.text < b.text ) ? -1 : ( a.text > b.text ) ? 1 : 0;
 		} );
+		SMD.tissues = tissues;
 	},
 	
-	fetchMaterials: function () {
-		const params = new URLSearchParams( {
-			origin: '*',
-			format: 'json',
-			formatversion: 2,
+	fetchMaterials: async function () {
+		const params = {
 			action: 'query',
-			generator: 'categorymembers',
-			gcmtitle: 'Category:SMD materials',
-			gcmlimit: 'max',
+			generator: 'search',
+			gsrsearch: 'incategory:"SMD materials" -incategory:"Automatic translations"',
+			gsrlimit: 'max',
 			prop: 'info',
 			inprop: 'url'
+		};
+		const data = await SMD.get( params );
+		const results = data.query.pages;
+		const materials = [];
+		for ( const result of results ) {
+			const material = {
+				type: 'material',
+				href: result.fullurl,
+				title: result.title,
+				text: result.title.replace( 'SMD/Materials/', '' )
+			};
+			materials.push( material );
+		}
+		// Sort alphabetically by text
+		materials.sort( function ( a, b ) {
+			return ( a.text < b.text ) ? -1 : ( a.text > b.text ) ? 1 : 0;
 		} );
-		const url = 'https://www.appropedia.org/w/api.php?' + params.toString();
-		return fetch( url ).then( response => response.json() ).then( response => {
-			const results = response.query.pages;
-			const materials = [];
-			for ( const result of results ) {
-				const material = {
-					type: 'material',
-					href: result.fullurl,
-					text: result.title.replace( 'SMD/Materials/', '' )
-				};
-				materials.push( material );
-			}
-			// Sort alphabetically by text
-			materials.sort( function ( a, b ) {
-				return ( a.text < b.text ) ? -1 : ( a.text > b.text ) ? 1 : 0;
-			} );
-			SMD.materials = materials;
-		} );
+		SMD.materials = materials;
 	},
 
-	fetchSimulations: function () {
+	fetchSimulations: async function () {
 		const conditions = [
 			'Category:SMD simulations',
 			'SMD reviewed::true'
@@ -399,41 +384,35 @@ const SMD = {
 			'SMD material',
 			'SMD developer'
 		];
-		const params = new URLSearchParams( {
-			origin: '*',
-			format: 'json',
+		const params = {
 			action: 'askargs',
 			conditions: conditions.join( '|' ),
 			printouts: printouts.join( '|' )
-		} );
-		const url = 'https://www.appropedia.org/w/api.php?' + params.toString();
-		return fetch( url ).then( response => response.json() ).then( response => {
-			const results = response.query.results;
-			const simulations = [];
-			for ( const [ key, result ] of Object.entries( results ) ) {
-				const simulation = {
-					type: 'simulation',
-					href: result.fullurl,
-					text: result.displaytitle,
-					tissue: result.printouts['SMD tissue'],
-					material: result.printouts['SMD material'],
-					developer: result.printouts['SMD developer']
-				}
-				simulations.push( simulation );
+		};
+		const data = await SMD.get( params );
+		const results = data.query.results;
+		const simulations = [];
+		for ( const [ key, result ] of Object.entries( results ) ) {
+			const simulation = {
+				type: 'simulation',
+				href: result.fullurl,
+				title: result.fulltext,
+				text: result.displaytitle || result.fulltext.replace( 'SMD/Simulations/', '' ),
+				tissue: result.printouts['SMD tissue'],
+				material: result.printouts['SMD material'],
+				developer: result.printouts['SMD developer']
 			}
-			// Sort alphabetically by tissue
-			simulations.sort( ( a, b ) => {
-				return ( a.tissue < b.tissue ) ? -1 : ( a.tissue > b.tissue ) ? 1 : 0;
-			} );
-			SMD.simulations = simulations;
+			simulations.push( simulation );
+		}
+		// Sort alphabetically by tissue
+		simulations.sort( ( a, b ) => {
+			return ( a.tissue < b.tissue ) ? -1 : ( a.tissue > b.tissue ) ? 1 : 0;
 		} );
+		SMD.simulations = simulations;
 	},
 
-	fetchTissue: function ( tissueName ) {
-		const params = new URLSearchParams( {
-			origin: '*',
-			format: 'json',
-			formatversion: 2,
+	fetchTissue: async function ( tissueName ) {
+		const params = {
 			action: 'query',
 			titles: 'SMD/Tissues/' + tissueName,
 			prop: 'info|pageimages|extracts',
@@ -441,22 +420,20 @@ const SMD = {
 			pithumbsize: 300,
 			explaintext: true,
 			exintro: true
-		} );
-		const url = 'https://www.appropedia.org/w/api.php?' + params.toString();
-		return fetch( url ).then( response => response.json() ).then( response => {
-			const page = response.query.pages[0];
-			const tissue = {
-				name: tissueName,
-				href: page.fullurl,
-				title: page.title,
-				image: page.thumbnail,
-				about: page.extract
-			};
-			return tissue;
-		} );
+		};
+		const data = await SMD.get( params );
+		const page = data.query.pages[0];
+		const tissue = {
+			name: tissueName,
+			href: page.fullurl,
+			title: page.title,
+			image: page.thumbnail,
+			about: page.extract
+		};
+		return tissue;
 	},
 
-	fetchTissueSimulations: function ( tissue ) {
+	fetchTissueSimulations: async function ( tissue ) {
 		const conditions = [
 			'Category:SMD simulations',
 			'SMD tissue::' + tissue,
@@ -470,39 +447,73 @@ const SMD = {
 			'SMD auditory',
 			'SMD olfactory'
 		];
-		const params = new URLSearchParams( {
-			origin: '*',
-			format: 'json',
+		const params = {
 			action: 'askargs',
 			conditions: conditions.join( '|' ),
 			printouts: printouts.join( '|' )
-		} ).toString();
-		return fetch( 'https://www.appropedia.org/w/api.php?' + params ).then( response => response.json() ).then( response => {
-			const results = response.query.results;
-			const simulations = [];
-			for ( const [ key, result ] of Object.entries( results ) ) {
-				const simulation = {
-					type: 'simulation',
-					text: result.displaytitle,
-					href: result.fullurl,
-					printouts: result.printouts
-				};
-				simulations.push( simulation );
-			}
-			return simulations;
-		} );
+		};
+		const data = await SMD.get( params );
+		const results = data.query.results;
+		const simulations = [];
+		for ( const [ key, result ] of Object.entries( results ) ) {
+			const simulation = {
+				type: 'simulation',
+				text: result.displaytitle,
+				href: result.fullurl,
+				printouts: result.printouts
+			};
+			simulations.push( simulation );
+		}
+		return simulations;
+	},
+
+	downloadPDF: async function () {
+
+		// Disable the button to prevent multiple clicks and hint the user that something is happening
+		const button = this;
+		const buttonText = button.textContent;
+		button.textContent = 'Generating...';
+		button.style.pointerEvents = 'none';
+
+		// Get the titles of the bookmarks
+		const titles = SMD.bookmarks.map( bookmark => bookmark.title );
+
+		// Generate the PDF
+		const url = 'https://www.appropedia.org/scripts/generatePDF.php?pages=' + titles.join( ',' );
+		const result = await fetch( url );
+		const bytes = await result.arrayBuffer();
+
+		// Download the PDF
+		const blob = new Blob( [ bytes ], { type: 'application/pdf' } );
+		const href = URL.createObjectURL( blob );
+		const a = document.createElement( 'a' );
+		a.href = href;
+		a.download = 'smd-bookarks.pdf';
+		document.body.appendChild( a );
+		a.click();
+		document.body.removeChild( a );
+		URL.revokeObjectURL( href );
+
+		// Re-enable the button
+		button.textContent = buttonText;
+		button.style.pointerEvents = '';
 	},
 
 	/**
-	 * Helper method to get a cookie
+	 * Helper method to do GET requests to the Appropedia Action API
 	 */
-	getCookie: function ( name ) {
-		const regex = new RegExp( '(^| )' + name + '=([^;]+)' );
-		const match = document.cookie.match( regex );
-		if ( match ) {
-			return match[2];
+	get: async function ( query ) {
+		query.origin = '*';
+		query.format = 'json';
+		query.formatversion = 2;
+		const queryString = new URLSearchParams( query ).toString();
+		const response = await fetch( 'https://www.appropedia.org/w/api.php?' + queryString );
+		if ( !response.ok ) {
+			// @todo Error handling
 		}
+		const data = await response.json();
+		return data;
 	}
-}
+};
 
 window.onload = SMD.init;
